ee0b798053934911b19fd4e2cab47ec5f6ec02f7
Siddhant <singhsiddhantakshat@gmail.com>
2025-11-10 06:42:13 -0600

    feat: first commit.

---
+++ voxa/voxaTests/voxaTests.swift
@@ -0,0 +1,17 @@
+//
+//  voxaTests.swift
+//  voxaTests
+//
+//  Created by Singh, Siddhant on 11/10/25.
+//
+
+import Testing
+@testable import voxa
+
+struct voxaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
